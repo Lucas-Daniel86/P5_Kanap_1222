a933e4b3d82cd95445e4d97763e17d0909c8dd01
Lucas Cantero <ldanielcantero@gmail.com>
2022-12-18 19:12:45 +0100

    page index dynamique ok

--- front/js/script.js
+++ front/js/script.js
@@ -2,33 +2,33 @@ fetch("http://localhost:3000/api/products")
   .then((res) => res.json())
   .then((data) => addProducts(data))
 
-function addProducts(ref) {
+function addProducts(refs) {
    //const _id = ref[0]._id
    //const imageUrl = ref[0].imageUrl
    //const altTxt = ref[0].altTxt
    //const name = ref[0].name
    //const description = ref[0].description
-   console.log(ref)
 
-   ref.forEach((sofa) => {
-      console.log("sofa: ", sofa)
+   refs.forEach((ref) => {
+   const {_id, imageUrl, altTxt, name, description} = ref;
+   const anchor = makeAnchor(_id);
+   const article = document.createElement("article");
+   const image = makeImage(imageUrl, altTxt);
+   const h3 = makeH3(name);
+   const p = makeParagraph(description);
 
-   const {_id, imageUrl, altTxt, name, description} = sofa
-   const anchor = makeAnchor(_id)
-   const article = document.createElement("article")
-   const image = makeImage(imageUrl, altTxt)
-   const h3 = makeH3(name)
-   const p = makeParagraph(description)
-
-   appendElementsToArticle(article, image, h3, p)
+   appendElementsToArticle(article, [image, h3, p]);
    appendArticleToAnchor(anchor, article)
    })
 }
 
-function appendElementsToArticle(article, image, h3, p) {
-   article.appendChild(image)
-   article.appendChild (h3)
-   article.appendChild(p)
+function appendElementsToArticle(article, array) {
+   array.forEach((item) => {
+      article.appendChild(item)
+   })
+   //article.appendChild(image);
+   //article.appendChild (h3);
+   //article.appendChild(p)
 }
 
 function makeAnchor(_id) {
@@ -42,7 +42,6 @@ function appendArticleToAnchor(anchor, article) {
    if (items != null) {
     items.appendChild(anchor)
     anchor.appendChild (article)
-    console.log("éléments ajoutés à items", items)
    }
 }
 function makeImage(imageUrl, altTxt) {
