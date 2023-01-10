6cb2126f53e743a8f3017abc05f49a9387a4b3da
Lucas Cantero <ldanielcantero@gmail.com>
2023-01-10 12:00:17 +0100

    Commentaires ok

--- front/js/script.js
+++ front/js/script.js
@@ -1,7 +1,8 @@
+// Appel de l'API pour récupérer les produits
 fetch("http://localhost:3000/api/products")
    .then((res) => res.json())
    .then((data) => addProducts(data));
-
+// Tableau des produits
 function addProducts(refs) {
    refs.forEach((ref) => {
       const { _id, imageUrl, altTxt, name, description } = ref;
@@ -15,19 +16,19 @@ function addProducts(refs) {
       appendArticleToAnchor(anchor, article);
    });
 }
-
+// Items du produit ajoutés dans l'élément <article>
 function appendElementsToArticle(article, array) {
    array.forEach((item) => {
       article.appendChild(item);
    });
 }
-
+// Création de l'élément <a> qui contient le lien du produit
 function makeAnchor(_id) {
    const anchor = document.createElement("a");
    anchor.href = "./product.html?_id=" + _id;
    return anchor;
 }
-
+// Récupération de la div "items" qui contient le lien et l'article du produit. l'élément <article> est à l'interieur de l'élément <a>
 function appendArticleToAnchor(anchor, article) {
    const items = document.querySelector("#items");
    if (items != null) {
@@ -35,21 +36,21 @@ function appendArticleToAnchor(anchor, article) {
       anchor.appendChild(article);
    }
 }
+// Création de l'élément <img> qui contient l'image et la description de l'image du produit.
 function makeImage(imageUrl, altTxt) {
    const image = document.createElement("img");
    image.src = imageUrl;
    image.alt = altTxt;
-   image.removeAttribute("title");
-   image.removeAttribute("style");
    return image;
 }
-
+// Création de l'élément <h3> qui contient le nom du produit
 function makeH3(name) {
    const h3 = document.createElement("h3");
    h3.textContent = name;
    h3.classList.add("productName");
    return h3;
 }
+// Création de l'élément <p> qui contient la description du produit.
 function makeParagraph(description) {
    const p = document.createElement("p");
    p.textContent = description;
